c6ae79259cdd4ddfbb6eb3c6f2428080a051af94
Hans Johnson <hans-johnson@uiowa.edu>
2011-05-17 21:25:18 -0500

    ENH:  Builds now work.

--- SuperBuild/External_SimpleITK.cmake
+++ SuperBuild/External_SimpleITK.cmake
@@ -2,18 +2,24 @@
 #  SimpleITK externalBuild
 #
 include(ExternalProject)
+
 ExternalProject_add(SimpleITK
   SOURCE_DIR SimpleITK
   BINARY_DIR SimpleITK-build
   GIT_REPOSITORY https://github.com/hjmjohnson/SimpleITK.git
-  GIT_TAG ConsolodateImageStatMeasurements
+  #  GIT_TAG ConsolodateImageStatMeasurements
+  GIT_TAG master
   UPDATE_COMMAND ""
   CMAKE_ARGS
     --no-warn-unused-cli
     ${ep_common_args}
-  -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
+  # SimpleITK does not work with shared libs turned on
+  #  -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
+  -DBUILD_SHARED_LIBS:BOOL=OFF
   -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
   -DITK_DIR:PATH=${ITK_DIR}
+  -DBUILD_EXAMPLES:BOOL=ON
+  -DBUILD_TESTING:BOOL=ON
   -DWRAP_PYTHON:BOOL=ON
   -DWRAP_TCL:BOOL=OFF
   -DWRAP_JAVA:BOOL=OFF
